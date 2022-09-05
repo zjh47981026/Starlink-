d230d776fbd2c26217345357c47e2249b0e7e820
zjh47981026 <zjh479810262@gmail.com>
2022-09-05 15:27:37 -0400

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2022 StarLink Tracker. All Rights Reserved. Website Made by Jiahao Zhang
+            </footer>
+        );
+    }
+}
+
+export default Footer;
+
---
+++ src/components/constants.js
@@ -0,0 +1,12 @@
+
+export const SAT_API_KEY = "VT6WQR-ZA2UKX-CJ98EA-4USD";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
+
+export const BASE_URL = "https://ec2-18-220-85-211.us-east-2.compute.amazonaws.com";
